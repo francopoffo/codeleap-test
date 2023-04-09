3395237f47812fd9bcac573d22ac5832c3b09eb2
Franco Poffo <franco_sutter@yahoo.com.br>
2023-04-09 16:32:00 -0300

    feat: Creating the Sign Up modal

---
+++ src/components/signupmodal.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import classes from "./signupmodal.module.css";
+
+const SignUpModal: React.FC = () => {
+  const [username, setUsername] = useState("");
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+  const [modal, setModal] = useState(true);
+
+  const onSubmitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    localStorage.setItem("username", username);
+    setModal(false);
+  };
+
+  if (modal) {
+    return (
+      <div className={classes.modal}>
+        <div className={classes.overlay}>
+          <form className={classes.signup} onSubmit={onSubmitHandler}>
+            <h2>Welcome to CodeLeap network!</h2>
+            <div>
+              <label htmlFor="username">Please enter your username</label>
+              <input
+                type="text"
+                placeholder="Your name"
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                  if (username.trim() != "") {
+                    setButtonDisabled(false);
+                  }
+                }}
+              ></input>
+            </div>
+            <button
+              type="submit"
+              disabled={buttonDisabled}
+              style={
+                buttonDisabled
+                  ? { background: "grey" }
+                  : { background: "#7695EC" }
+              }
+            >
+              ENTER
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
+};
+
+export default SignUpModal;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Roboto } from "next/font/google";
+import SignUpModal from "@/components/signupmodal";
 
 const roboto = Roboto({
   weight: ["400", "700"],
@@ -15,7 +16,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={roboto.className}></main>
+      <main className={roboto.className}>
+        <SignUpModal />
+      </main>
     </>
   );
 }
